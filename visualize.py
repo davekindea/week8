639ed5592ca3395bbf82fe502b4a5bd08977e971
davekindea <davekindea1993@gmail.com>
2025-03-09 09:58:50 -0700

    Update configuration

--- visualize.py
+++ visualize.py
@@ -31,7 +31,7 @@ def plot_loss(history):
     plt.ylabel("Loss")
     plt.show()
 
-# TODO: Review: Review: Review: Review: Review implementation
+# TODO: Review: Review: Review: Review: Review: Review implementation
 
 # Last updated: 2025-12-11
 
